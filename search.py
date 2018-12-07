abed2f0abb91a0113feb9244d119c762640418c7
Rayson Lim Jun Kai <rayson@jublia.com>
2018-12-07 20:16:02 +0800

    clean up folder by deleting useless stuff

--- search.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-file = sys.argv[1]
-with open(file) as f:
-    for line in f:
-        word = line.strip().split(" ")[0]
-        if word == "_START" or word == "_STOP":
-            print("NOT UNIQUE")
-            break
